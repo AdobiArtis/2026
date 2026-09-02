948995b10e42c6da9189b24b7984f1bb7a5a7e6d
AdobiArtis <laila.dalla.torre@gmail.com>
2026-09-02 21:50:11 +0200

    Create programme.js

---
+++ programme.js
@@ -0,0 +1,241 @@
+const locations = [
+
+  {
+    id: 1,
+    name: "Casa Podda",
+    category: "Arte",
+
+    latitude: 39.8148,
+    longitude: 9.2720,
+
+    description:
+      "Esposizioni e incontri con gli artisti."
+  },
+
+
+  {
+    id: 2,
+    name: "Area Bambini",
+    category: "Bambini",
+
+    latitude: 39.8153,
+    longitude: 9.2725,
+
+    description:
+      "Attività creative, giochi e animazione."
+  },
+
+
+  {
+    id: 3,
+    name: "Area Spettacoli",
+    category: "Spettacoli",
+
+    latitude: 39.8150,
+    longitude: 9.2714,
+
+    description:
+      "Performance e spettacoli."
+  },
+
+
+  {
+    id: 4,
+    name: "Food & Drink",
+    category: "Food",
+
+    latitude: 39.8145,
+    longitude: 9.2717,
+
+    description:
+      "Area ristoro AdobiArtis."
+  },
+
+
+  {
+    id: 5,
+    name: "Info Point",
+    category: "Servizi",
+
+    latitude: 39.8149,
+    longitude: 9.2718,
+
+    description:
+      "Informazioni sulla manifestazione."
+  }
+
+];
+
+
+
+const programme = [
+
+  /* SABATO */
+
+  {
+    day: "sabato",
+
+    time: "10:00",
+
+    title:
+      "Apertura AdobiArtis",
+
+    category:
+      "Arte",
+
+    locationId: 1
+  },
+
+
+  {
+    day: "sabato",
+
+    time: "11:00",
+
+    title:
+      "Dipingi con noi",
+
+    category:
+      "Bambini",
+
+    locationId: 2
+  },
+
+
+  {
+    day: "sabato",
+
+    time: "15:00",
+
+    title:
+      "Trucco bambini",
+
+    category:
+      "Bambini",
+
+    locationId: 2
+  },
+
+
+  {
+    day: "sabato",
+
+    time: "17:00",
+
+    title:
+      "Giocoliere",
+
+    category:
+      "Spettacoli",
+
+    locationId: 3
+  },
+
+
+  {
+    day: "sabato",
+
+    time: "19:00",
+
+    title:
+      "Sputafuoco",
+
+    category:
+      "Spettacoli",
+
+    locationId: 3
+  },
+
+
+  {
+    day: "sabato",
+
+    time: "21:00",
+
+    title:
+      "Musica dal vivo",
+
+    category:
+      "Musica",
+
+    locationId: 3
+  },
+
+
+  /* DOMENICA */
+
+  {
+    day: "domenica",
+
+    time: "10:00",
+
+    title:
+      "Apertura esposizioni",
+
+    category:
+      "Arte",
+
+    locationId: 1
+  },
+
+
+  {
+    day: "domenica",
+
+    time: "11:30",
+
+    title:
+      "Costruzione LEGO",
+
+    category:
+      "Bambini",
+
+    locationId: 2
+  },
+
+
+  {
+    day: "domenica",
+
+    time: "16:00",
+
+    title:
+      "Pagliacci",
+
+    category:
+      "Spettacoli",
+
+    locationId: 3
+  },
+
+
+  {
+    day: "domenica",
+
+    time: "18:00",
+
+    title:
+      "Giocoliere",
+
+    category:
+      "Spettacoli",
+
+    locationId: 3
+  },
+
+
+  {
+    day: "domenica",
+
+    time: "21:00",
+
+    title:
+      "Chiusura AdobiArtis",
+
+    category:
+      "Musica",
+
+    locationId: 3
+  }
+
+];
