a7b9698edc88e6111a4f8fb68b713074057a2684
AdobiArtis <laila.dalla.torre@gmail.com>
2026-09-03 01:26:29 +0200

    Update programme.js

--- programme.js
+++ programme.js
@@ -168,66 +168,7 @@ const programme = [
     description: ""
   },
 
-  {
-    day: "sabato",
-    time: "12:00",
-    endTime: "24:00",
-    title: "Food & Drinks",
-    category: "FD",
-    placeId: "P05",
-    description: ""
-  },
-
-  {
-    day: "sabato",
-    time: "15:00",
-    endTime: "17:00",
-    title: "Trucco bambini",
-    category: "BA",
-    placeId: "P02",
-    description: ""
-  },
-
-  {
-    day: "sabato",
-    time: "17:00",
-    endTime: "17:45",
-    title: "Giocoliere",
-    category: "SP",
-    placeId: "P03",
-    description: ""
-  },
-
-  {
-    day: "sabato",
-    time: "18:00",
-    endTime: "19:00",
-    title: "Poesia e letture",
-    category: "PL",
-    placeId: "P01",
-    description: ""
-  },
-
-  {
-    day: "sabato",
-    time: "19:00",
-    endTime: "19:30",
-    title: "Sputafuoco",
-    category: "SP",
-    placeId: "P03",
-    description: ""
-  },
-
-  {
-    day: "sabato",
-    time: "21:00",
-    endTime: "23:00",
-    title: "Musica dal vivo",
-    category: "MU",
-    placeId: "P04",
-    description: ""
-  },
-
+ 
 
   /* ---------- DOMENICA ---------- */
 
@@ -250,64 +191,6 @@ const programme = [
     placeId: "P09",
     description: ""
       },  
-  {
-    day: "domenica",
-    time: "10:00",
-    endTime: "20:00",
-    title: "Artisti all'opera",
-    category: "AO",
-    placeId: "P01",
-    description: ""
-  },
-
-  {
-    day: "domenica",
-    time: "11:30",
-    endTime: "13:00",
-    title: "Costruzione LEGO",
-    category: "BA",
-    placeId: "P02",
-    description: ""
-  },
-
-  {
-    day: "domenica",
-    time: "12:00",
-    endTime: "22:00",
-    title: "Food & Drinks",
-    category: "FD",
-    placeId: "P05",
-    description: ""
-  },
-
-  {
-    day: "domenica",
-    time: "16:00",
-    endTime: "16:45",
-    title: "Pagliacci",
-    category: "SP",
-    placeId: "P03",
-    description: ""
-  },
-
-  {
-    day: "domenica",
-    time: "18:00",
-    endTime: "18:45",
-    title: "Giocoliere",
-    category: "SP",
-    placeId: "P03",
-    description: ""
-  },
-
-  {
-    day: "domenica",
-    time: "20:00",
-    endTime: "22:00",
-    title: "Musica dal vivo",
-    category: "MU",
-    placeId: "P04",
-    description: ""
-  }
+ 
 
 ];
