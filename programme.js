91b479916ce8e20c7ffd6dd4a14332a9929bb6ed
AdobiArtis <laila.dalla.torre@gmail.com>
2026-09-03 00:50:17 +0200

    Update programme.js

--- programme.js
+++ programme.js
@@ -1,121 +1,230 @@
-const locations = [
+/* =========================
+   ADOBIARTIS 2026
+   PROGRAMME DATA
+========================= */
+
+
+/* =========================
+   CATEGORIES
+========================= */
+
+const categories = [
+  {
+    id: "MU",
+    name: "Musica"
+  },
+  {
+    id: "PL",
+    name: "Poesia / Lettura"
+  },
+  {
+    id: "LA",
+    name: "Laboratorio"
+  },
+  {
+    id: "SP",
+    name: "Spettacolo"
+  },
+  {
+    id: "BA",
+    name: "Bambini"
+  },
+  {
+    id: "AO",
+    name: "Artisti all'opera"
+  },
+  {
+    id: "FD",
+    name: "Food & Drinks"
+  }
+];
+
+
+/* =========================
+   PLACES
+   Replace these with the
+   final places from your map
+========================= */
+
+const places = [
 
   {
-    id: 1,
+    id: "P01",
     name: "Casa Podda"
   },
 
   {
-    id: 2,
+    id: "P02",
     name: "Area Bambini"
   },
 
   {
-    id: 3,
-    name: "Area Spettacoli"
+    id: "P03",
+    name: "Piazza San Valentino"
   },
 
   {
-    id: 4,
-    name: "Area Food & Drink"
+    id: "P04",
+    name: "Palco principale"
+  },
+
+  {
+    id: "P05",
+    name: "Area Food & Drinks"
   }
 
 ];
 
 
+/* =========================
+   PROGRAMME
+========================= */
+
 const programme = [
 
-  /* SABATO */
+  /* ---------- SABATO ---------- */
 
   {
     day: "sabato",
     time: "10:00",
-    title: "Apertura AdobiArtis",
-    category: "Arte",
-    locationId: 1
+    endTime: "20:00",
+    title: "Artisti all'opera",
+    category: "AO",
+    placeId: "P01",
+    description: ""
   },
 
   {
     day: "sabato",
     time: "11:00",
+    endTime: "12:00",
     title: "Dipingi con noi",
-    category: "Bambini",
-    locationId: 2
+    category: "BA",
+    placeId: "P02",
+    description: ""
+  },
+
+  {
+    day: "sabato",
+    time: "12:00",
+    endTime: "24:00",
+    title: "Food & Drinks",
+    category: "FD",
+    placeId: "P05",
+    description: ""
   },
 
   {
     day: "sabato",
     time: "15:00",
+    endTime: "17:00",
     title: "Trucco bambini",
-    category: "Bambini",
-    locationId: 2
+    category: "BA",
+    placeId: "P02",
+    description: ""
   },
 
   {
     day: "sabato",
     time: "17:00",
+    endTime: "17:45",
     title: "Giocoliere",
-    category: "Spettacoli",
-    locationId: 3
+    category: "SP",
+    placeId: "P03",
+    description: ""
+  },
+
+  {
+    day: "sabato",
+    time: "18:00",
+    endTime: "19:00",
+    title: "Poesia e letture",
+    category: "PL",
+    placeId: "P01",
+    description: ""
   },
 
   {
     day: "sabato",
     time: "19:00",
+    endTime: "19:30",
     title: "Sputafuoco",
-    category: "Spettacoli",
-    locationId: 3
+    category: "SP",
+    placeId: "P03",
+    description: ""
   },
 
   {
     day: "sabato",
     time: "21:00",
+    endTime: "23:00",
     title: "Musica dal vivo",
-    category: "Musica",
-    locationId: 3
+    category: "MU",
+    placeId: "P04",
+    description: ""
   },
 
 
-  /* DOMENICA */
+  /* ---------- DOMENICA ---------- */
 
   {
     day: "domenica",
     time: "10:00",
-    title: "Apertura esposizioni",
-    category: "Arte",
-    locationId: 1
+    endTime: "20:00",
+    title: "Artisti all'opera",
+    category: "AO",
+    placeId: "P01",
+    description: ""
   },
 
   {
     day: "domenica",
     time: "11:30",
+    endTime: "13:00",
     title: "Costruzione LEGO",
-    category: "Bambini",
-    locationId: 2
+    category: "BA",
+    placeId: "P02",
+    description: ""
+  },
+
+  {
+    day: "domenica",
+    time: "12:00",
+    endTime: "22:00",
+    title: "Food & Drinks",
+    category: "FD",
+    placeId: "P05",
+    description: ""
   },
 
   {
     day: "domenica",
     time: "16:00",
+    endTime: "16:45",
     title: "Pagliacci",
-    category: "Spettacoli",
-    locationId: 3
+    category: "SP",
+    placeId: "P03",
+    description: ""
   },
 
   {
     day: "domenica",
     time: "18:00",
+    endTime: "18:45",
     title: "Giocoliere",
-    category: "Spettacoli",
-    locationId: 3
+    category: "SP",
+    placeId: "P03",
+    description: ""
   },
 
   {
     day: "domenica",
-    time: "21:00",
-    title: "Chiusura AdobiArtis",
-    category: "Musica",
-    locationId: 3
+    time: "20:00",
+    endTime: "22:00",
+    title: "Musica dal vivo",
+    category: "MU",
+    placeId: "P04",
+    description: ""
   }
 
 ];
